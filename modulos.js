3e915f1689a254d68c402b58f4e5a4fd446c91c7
Gustavo1701 <gustavojr.lg.10@gmail.com>
2025-01-02 20:56:24 -0300

    Atualizando modulos

---
+++ modulos.js
@@ -0,0 +1,9 @@
+// 1 - importanto módulo nativo do Node.js, utilizando o CommonJs (require())
+const os = require('os');
+
+console.log('Hostname:', os.hostname());
+
+// 2 - Módulo personalizado, criado em outro arquivo
+const ipv4 = require('./ipv4');
+
+console.log('Ipv4:', ipv4);
